c7e93dd97fdb32c295266f9578c8de003c0c6d1f
talshmuel <you@example.com>
2024-08-05 13:36:40 +0300

    5.8.24 13:26

--- src/main/java/newsGuardServer/Server.java
+++ src/main/java/newsGuardServer/Server.java
@@ -16,22 +16,5 @@ public class Server {
 
     public static void main(String[] args) throws SQLException {
         SpringApplication.run(Server.class, args);
-        check();
     }
-
-    public static void check() throws SQLException {
-        ArrayList<String> genre = new ArrayList<>();
-        genre.add("1");
-        ArrayList<Integer> reliability = new ArrayList<>();
-        reliability.add(1);
-        ArrayList<String> countries = new ArrayList<>();
-        countries.add("Holand");
-        NewUserDTO userDTO = new NewUserDTO("tal", "shmuel", "israel", "nitzan@gmail.com",
-                "1234", "http:wdqwd", "0525440634",
-                genre, reliability, countries,
-                true);
-        Engine engine = new Engine();
-        engine.createNewUser(userDTO);
-    }
-
 }
