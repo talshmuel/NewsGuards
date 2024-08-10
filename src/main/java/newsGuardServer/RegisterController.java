4fc8bd13f696d2f9c566a2fc3c9a44a0c1ee4577
nitzansde <nitzansdeor24@gmail.com>
2024-08-10 14:30:10 +0300

    restore user

--- src/main/java/newsGuardServer/RegisterController.java
+++ src/main/java/newsGuardServer/RegisterController.java
@@ -5,10 +5,9 @@ import logic.engine.Engine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:5173")
 
 @RestController
 @RequestMapping("/register")
