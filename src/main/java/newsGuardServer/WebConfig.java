bafc42bdafd56fde2b6f22abc143b169309d550c
talshmuel <you@example.com>
2024-09-08 21:30:58 +0300

    cors policy changed to the cloud url

--- src/main/java/newsGuardServer/WebConfig.java
+++ src/main/java/newsGuardServer/WebConfig.java
@@ -7,14 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("**")
-                .allowedOrigins("https://news-guard.vercel.app") // Your frontend origin
+        registry.addMapping("/**")
+                .allowedOrigins("https://news-guard.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+
+
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
