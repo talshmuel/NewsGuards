0580397a08fe003997d6193fe221eb2031f7ad81
nitzansdeor1 <ronitsdeor@gmail.com>
2024-09-09 18:55:16 +0300

    try to fix image upload

---
+++ src/main/java/newsGuardServer/FirebaseConfig.java
@@ -0,0 +1,32 @@
+package newsGuardServer;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("C:/Users/User/Downloads/newsguardimages-firebase-adminsdk-9rb7u-448351a53e.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
